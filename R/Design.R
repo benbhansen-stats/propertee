fe1053e96fdd5993c0f858ab2e37edba498cae7e
Josh Errickson <josh@errickson.net>
2021-05-12 06:15:29 -0400

    Move from factor to character columnIndex. Improved validation.

--- R/Design.R
+++ R/Design.R
@@ -1,61 +1,73 @@
 setClass("Design",
          slots = c(structure = "data.frame",
-                   columnIndex = "factor",
+                   columnIndex = "character",
                    type = "character"))
 
 setValidity("Design", function(object) {
   if (any(dim(object@structure) == 0)) {
-    "@structure must have positive dimensions"
-  } else if (ncol(object@structure) != length(object@columnIndex)) {
-    "@columnIndex does not agree with number of columns in @structure"
-  } else {
-    #TODO: Add validity for `type`
-    TRUE
+    return("@structure must have positive dimensions")
   }
+  if (ncol(object@structure) != length(object@columnIndex)) {
+    return("@columnIndex does not agree with number of columns in @structure")
+  }
+  if (!all(object@columnIndex %in% c("t", "c", "b", "f"))) {
+    wrong <- object@columnIndex[!object@columnIndex %in% c("t", "c", "b", "f")]
+    return(paste("@columnIndex design elements must be [t,c,b,f]. unknown elements:",
+                 paste(wrong, collapse = ", ")))
+  }
+  if (!object@type %in% c("RCT", "RD", "Obs")) {
+    return(paste("@type must be one of [RCT,RD,Obs]. unknown @type:", object@type))
+  }
+  TRUE
 })
 
 
-New_Design <- function(form, data, subset = NULL) {
+New_Design <- function(form, data, type, subset = NULL) {
   if (!is.null(subset)) {
     data <- subset(data, subset = subset)
   }
 
   m <- as.data.frame(as.matrix(model.frame(form, data)))
 
-  index <- factor(rep("t", ncol(m)),
-                  levels = c("t", "c", "b", "f"))
+  index <- rep("t", ncol(m))
 
   # Handle clusters
   clusters <- grepl("^cluster", colnames(m))
-  index[which(clusters)] <- "c"
-  cvars <- colnames(m)[clusters][1]
-  cvars <- sub("^cluster\\(", "", cvars)
-  cvars <- sub("\\)[\\.0-9]*$", "", cvars)
-  cvars <- gsub(" ", "", cvars)
-  colnames(m)[clusters] <- strsplit(cvars, ",")[[1]]
+  if (any(clusters)) {
+    index[which(clusters)] <- "c"
+    cvars <- colnames(m)[clusters][1]
+    cvars <- sub("^cluster\\(", "", cvars)
+    cvars <- sub("\\)[\\.0-9]*$", "", cvars)
+    cvars <- gsub(" ", "", cvars)
+    colnames(m)[clusters] <- strsplit(cvars, ",")[[1]]
+  }
 
   # Handle blocks
   blocks <- grepl("^block", colnames(m))
-  index[which(blocks)] <- "b"
-  bvars <- colnames(m)[blocks][1]
-  bvars <- sub("^block\\(", "", bvars)
-  bvars <- sub("\\)[\\.0-9]*$", "", bvars)
-  bvars <- gsub(" ", "", bvars)
-  colnames(m)[blocks] <- strsplit(bvars, ",")[[1]]
+  if (any(blocks)) {
+    index[which(blocks)] <- "b"
+    bvars <- colnames(m)[blocks][1]
+    bvars <- sub("^block\\(", "", bvars)
+    bvars <- sub("\\)[\\.0-9]*$", "", bvars)
+    bvars <- gsub(" ", "", bvars)
+    colnames(m)[blocks] <- strsplit(bvars, ",")[[1]]
+  }
 
   # Handle forcing
   forcings <- grepl("^forcing", colnames(m))
-  index[which(forcings)] <- "f"
-  fvars <- colnames(m)[forcings][1]
-  fvars <- sub("^forcing\\(", "", fvars)
-  fvars <- sub("\\)[\\.0-9]*$", "", fvars)
-  fvars <- gsub(" ", "", fvars)
-  colnames(m)[forcings] <- strsplit(fvars, ",")[[1]]
+  if (any(forcings)) {
+    index[which(forcings)] <- "f"
+    fvars <- colnames(m)[forcings][1]
+    fvars <- sub("^forcing\\(", "", fvars)
+    fvars <- sub("\\)[\\.0-9]*$", "", fvars)
+    fvars <- gsub(" ", "", fvars)
+    colnames(m)[forcings] <- strsplit(fvars, ",")[[1]]
+  }
 
   new("Design",
       structure = m,
       columnIndex = index,
-      type = "None")
+      type = type)
 }
 
 ##' Generates an RCT Design object with the given specifications.
@@ -71,9 +83,7 @@ New_Design <- function(form, data, subset = NULL) {
 RCT_Design <- function(formula, data, subset = NULL) {
   checkDesignFormula(formula)
 
-  design <- New_Design(formula, data, subset = subset)
-
-  design@type <- "RCT"
+  design <- New_Design(formula, data, type = "RCT", subset = subset)
   return(design)
 }
 
@@ -91,9 +101,7 @@ RCT_Design <- function(formula, data, subset = NULL) {
 RD_Design <- function(formula, data, subset = NULL) {
   checkDesignFormula(formula, allowForcing = TRUE)
 
-  design <- New_Design(formula, data, subset = subset)
-
-  design@type <- "RD"
+  design <- New_Design(formula, data, type = "RD", subset = subset)
   return(design)
 }
 
@@ -110,8 +118,6 @@ RD_Design <- function(formula, data, subset = NULL) {
 Obs_Design <- function(formula, data, subset = NULL) {
   checkDesignFormula(formula)
 
-  design <- New_Design(formula, data, subset = subset)
-
-  design@type <- "Obs"
+  design <- New_Design(formula, data, type = "Obs", subset = subset)
   return(design)
 }
