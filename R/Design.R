ead7554ab5568ab096013fb1008c94cb4d9c95a6
Josh Errickson <josh@errickson.net>
2021-05-11 21:14:30 -0400

    Separate *_Design objects and DRY. Also some documentation.

--- R/Design.R
+++ R/Design.R
@@ -1,6 +1,7 @@
 setClass("Design",
          slots = c(structure = "data.frame",
-                   columnIndex = "factor"))
+                   columnIndex = "factor",
+                   type = "character"))
 
 setValidity("Design", function(object) {
   if (any(dim(object@structure) == 0)) {
@@ -8,17 +9,40 @@ setValidity("Design", function(object) {
   } else if (ncol(object@structure) != length(object@columnIndex)) {
     "@columnIndex does not agree with number of columns in @structure"
   } else {
+    #TODO: Add validity for `type`
     TRUE
   }
 })
 
-RCT_Design <- function(form, data, subset = NULL) {
+##' These are special function used only in the definition of Design class
+##' objects. They identify the clusters, blocks and forcing variables.
+##'
+##' @title Special terms in Design
+##' @param ... any number of variables of the same length.
+##' @return the variables with appropriate labels
+##' @export
+##' @rdname DesignSpecials
+forcing <- block <- cluster <- function (...)
+{
+  #browser()
+  allf <- list(...)
+  do.call(cbind, allf)
+}
+
+##' @rdname DesignSpecials
+##' @export
+cluster <- block
+
+##' @rdname DesignSpecials
+##' @export
+forcing <- block
+
+New_Design <- function(form, data, subset = NULL) {
   if (!is.null(subset)) {
-    d <- subset(data, subset = subset)
+    data <- subset(data, subset = subset)
   }
+
   m <- as.data.frame(as.matrix(model.frame(form, data)))
-  # TODO: Ensure only one `cluster()` and only one `block()` in formula
-  tt <- terms(form, c("cluster", "block"))
 
   index <- factor(rep("t", ncol(m)),
                   levels = c("t", "c", "b", "f"))
@@ -52,12 +76,96 @@ RCT_Design <- function(form, data, subset = NULL) {
 
   new("Design",
       structure = m,
-      columnIndex = index)
+      columnIndex = index,
+      type = "None")
 }
 
-forcing <- block <- cluster <- function (...)
-{
-  #browser()
-  allf <- list(...)
-  do.call(cbind, allf)
+##' Generates an RCT Design object with the given specifications.
+##'
+##' The `formula` must include `cluster()` to identify the units of assignment
+##' (one or more variables), it may optionally contain `strata()` as well.
+##' @title Specify RCT Design
+##' @param formula defines the design components
+##' @param data the data set.
+##' @param subset optionally subset the data before creating the design object
+##' @return a Design object of type "RCT" for use in further analysis
+##' @export
+RCT_Design <- function(formula, data, subset = NULL) {
+  checkDesignFormula(formula)
+
+  design <- New_Design(formula, data, subset = subset)
+
+  design@type <- "RCT"
+  return(design)
+}
+
+##' Generates an RD Design object with the given specifications.
+##'
+##' The `formula` must include `cluster()` to identify the units of assignment
+##' (one or more variables), it may optionally contain `strata()` and/or
+##' `forcing()` as well.
+##' @title Specify Regression Discontinuity Design
+##' @param formula defines the design components
+##' @param data the data set.
+##' @param subset optionally subset the data before creating the design object
+##' @return a Design object of type "RD" for use in further analysis
+##' @export
+RD_Design <- function(formula, data, subset = NULL) {
+  checkDesignFormula(formula, allowForcing = TRUE)
+
+  design <- New_Design(formula, data, subset = subset)
+
+  design@type <- "RD"
+  return(design)
+}
+
+##' Generates an Observationl Data Design object with the given specifications.
+##'
+##' The `formula` must include `cluster()` to identify the units of assignment
+##' (one or more variables), it may optionally contain `strata()` as well.
+##' @title Specify Observational Data Design
+##' @param formula defines the design components
+##' @param data the data set.
+##' @param subset optionally subset the data before creating the design object
+##' @return a Design object of type "Obs" for use in further analysis
+##' @export
+Obs_Design <- function(formula, data, subset = NULL) {
+  checkDesignFormula(formula)
+
+  design <- New_Design(formula, data, subset = subset)
+
+  design@type <- "Obs"
+  return(design)
+}
+
+# Perform checks on formula for creation of Design.
+# Checks performed:
+# - Ensure presence of cluster()
+# - Disallow multiple cluster(), block(), or forcing() terms
+# - Disallow forcing() unless in RDD
+checkDesignFormula <- function(form, allowForcing = FALSE) {
+  tt <- terms(form, c("cluster", "block", "forcing"))
+  specials <- attr(tt, "specials")
+
+  if (attr(tt, "response") == 0) {
+    stop("Must specify a treatment variable as the left side of the formula.")
+  }
+
+  if (is.null(specials$cluster)) {
+    stop("Must specify at least one clustering variable.")
+  } else if (length(specials$cluster) > 1) {
+    stop("Specify only one cluster() (cluster() can take multiple variables).")
+  }
+
+  if (!is.null(specials$block) && length(specials$block) > 1) {
+    stop("Specify only one block() (block() can take multiple variables).")
+  }
+
+  if (!allowForcing && !is.null(attr(tt, "specials")$forcing)) {
+    stop("forcing() only allowed in RD_Design")
+  } else if (allowForcing && length(specials$forcing) > 1) {
+    stop("Specify only one forcing() (forcing() can take multiple variables).")
+  }
+
+  TRUE
 }
