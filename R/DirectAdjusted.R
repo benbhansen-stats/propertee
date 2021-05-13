eb1687929c4797425a888461dced84f45afbca83
Josh Errickson <josh@errickson.net>
2021-05-12 21:02:39 -0400

    validity checker for DirectAdjusted

--- R/DirectAdjusted.R
+++ R/DirectAdjusted.R
@@ -3,6 +3,14 @@ DirectAdjusted <- setClass("DirectAdjusted",
                            slots = c(Design = "Design",
                                      target = "character"))
 
+setValidity("DirectAdjusted", function(object) {
+  if (length(object@target) != 1 || !object@target %in% c("ett", "ate")) {
+    return(paste("@target must be one of [ett, ate]. unknown @target:",
+                 paste(object@target, collapse = " ")))
+  }
+  TRUE
+})
+
 #' @export
 print.DirectAdjusted <- function(x, digits = max(3L, getOption("digits") - 3L), ...)  {
   stats:::print.lm(x, digits = digits, ...)
