d0c3971ad58f33019b98fa7d5e1cdcec1c6db65d
Josh Errickson <josh@errickson.net>
2022-01-27 09:38:19 -0500

    Importing na.pass

--- R/flexida_package.R
+++ R/flexida_package.R
@@ -1,5 +1,5 @@
 #' @importFrom utils getS3method methods
-#' @importFrom stats model.frame terms
+#' @importFrom stats model.frame terms na.pass
 #' @importFrom methods as is new validObject show
 NULL
 #> NULL
