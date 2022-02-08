6e151023c15e31915e96e878a09e1da9761254b7
Josh Errickson <josh@errickson.net>
2022-02-08 12:10:07 -0500

    Lots of whitespace cleanups

--- R/WeightedDesign.R
+++ R/WeightedDesign.R
@@ -41,12 +41,12 @@ ett <- function(design, data = NULL, unitOfAssignmentIds = NULL) {
   .treatment_concordance(design, data)
 
   #### generate weights
-  
+
   tx_vec <- design@structure[design@columnIndex == "t"][[1]]
   ind_tx <- levels(tx_vec)[2]
-  
+
   # If no block is specified, then E_Z is the proportion of clusters who receive
-  # the treatment. 
+  # the treatment.
   # If a block is specified, then E_Z varies by block and is the proportion
   # of clusters within the block that receive the treatment.
   if(!("b" %in% names(table(design@columnIndex)))){
@@ -55,27 +55,27 @@ ett <- function(design, data = NULL, unitOfAssignmentIds = NULL) {
     E_Z <- mean(cluster_df$Tx)
     weights <- cluster_df$Tx + (1 - cluster_df$Tx) * E_Z / (1 - E_Z)
   } else{
-    # Create a data frame at the block level with the block id, the number of 
-    # clusters within each block, and the number of clusters receiving the 
+    # Create a data frame at the block level with the block id, the number of
+    # clusters within each block, and the number of clusters receiving the
     # treatment within each block.
-    # Then, calculate E_Z. 
-    
-    block_df <- data.frame(blockid = names(table(design@structure[design@columnIndex == "b"])), 
+    # Then, calculate E_Z.
+
+    block_df <- data.frame(blockid = names(table(design@structure[design@columnIndex == "b"])),
                      block_units = as.numeric(table(design@structure[design@columnIndex == "b"])),
                      tx_units = tapply(as.numeric(tx_vec == ind_tx),
-                                       design@structure[design@columnIndex == "b"], 
+                                       design@structure[design@columnIndex == "b"],
                                        FUN = sum))
     block_df$E_Z <- block_df$tx_units / block_df$block_units
-    
+
     # Create a cluster-level data frame that merges design structure with block
-    # data frame. Add variable Tx that converts treatment to 0/1 for calculation 
-    # of ETT weights. 
-    cluster_df <- merge(design@structure, block_df, 
-                        by.x = colnames(design@structure[which(design@columnIndex == "b")]), 
+    # data frame. Add variable Tx that converts treatment to 0/1 for calculation
+    # of ETT weights.
+    cluster_df <- merge(design@structure, block_df,
+                        by.x = colnames(design@structure[which(design@columnIndex == "b")]),
                         by.y = "blockid")
     cluster_df$Tx <- as.numeric(cluster_df[,names(design@structure[design@columnIndex == "t"])] ==
-                                  ind_tx)    
-    weights <- cluster_df$Tx + (1 - cluster_df$Tx) * cluster_df$E_Z / (1 - cluster_df$E_Z) 
+                                  ind_tx)
+    weights <- cluster_df$Tx + (1 - cluster_df$Tx) * cluster_df$E_Z / (1 - cluster_df$E_Z)
   }
   .join_design_weights(weights, design, target = "ett", data = data)
 }
@@ -96,9 +96,9 @@ ate <- function(design, data = NULL, unitOfAssignmentIds = NULL) {
   #### generate weights
   tx_vec <- design@structure[design@columnIndex == "t"][[1]]
   ind_tx <- levels(tx_vec)[2]
-  
+
   # If no block is specified, then E_Z is the proportion of clusters who receive
-  # the treatment. 
+  # the treatment.
   # If a block is specified, then E_Z varies by block and is the proportion
   # of clusters within the block that receive the treatment.
   if(!("b" %in% names(table(design@columnIndex)))){
@@ -107,28 +107,28 @@ ate <- function(design, data = NULL, unitOfAssignmentIds = NULL) {
     E_Z <- mean(cluster_df$Tx)
     weights <- cluster_df$Tx / E_Z + (1 - cluster_df$Tx) / (1 - E_Z)
   } else{
-    # Create a data frame at the block level with the block id, the number of 
-    # clusters within each block, and the number of clusters receiving the 
+    # Create a data frame at the block level with the block id, the number of
+    # clusters within each block, and the number of clusters receiving the
     # treatment within each block.
-    # Then, calculate E_Z. 
-    
-    block_df <- data.frame(blockid = names(table(design@structure[design@columnIndex == "b"])), 
+    # Then, calculate E_Z.
+
+    block_df <- data.frame(blockid = names(table(design@structure[design@columnIndex == "b"])),
                            block_units = as.numeric(table(design@structure[design@columnIndex == "b"])),
                            tx_units = tapply(as.numeric(tx_vec == ind_tx),
-                                             design@structure[design@columnIndex == "b"], 
+                                             design@structure[design@columnIndex == "b"],
                                              FUN = sum))
     block_df$E_Z <- block_df$tx_units / block_df$block_units
-    
+
     # Create a cluster-level data frame that merges design structure with block
-    # data frame. Add variable Tx that converts treatment to 0/1 for calculation 
-    # of ATE weights. 
-    cluster_df <- merge(design@structure, block_df, 
-                        by.x = colnames(design@structure[which(design@columnIndex == "b")]), 
+    # data frame. Add variable Tx that converts treatment to 0/1 for calculation
+    # of ATE weights.
+    cluster_df <- merge(design@structure, block_df,
+                        by.x = colnames(design@structure[which(design@columnIndex == "b")]),
                         by.y = "blockid")
     cluster_df$Tx <- as.numeric(cluster_df[,names(design@structure[design@columnIndex == "t"])] ==
                                              ind_tx)
-    
-    weights <- cluster_df$Tx / cluster_df$E_Z + (1 - cluster_df$Tx) / (1 - cluster_df$E_Z) 
+
+    weights <- cluster_df$Tx / cluster_df$E_Z + (1 - cluster_df$Tx) / (1 - cluster_df$E_Z)
   }
 
   .join_design_weights(weights, design, target = "ate", data = data)
