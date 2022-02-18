962f55881335348ca614215abf663083aadcce50
Josh Errickson <josh@errickson.net>
2022-02-18 11:20:22 -0500

    Relocate function

--- R/WeightedDesign.R
+++ R/WeightedDesign.R
@@ -16,46 +16,6 @@ setValidity("WeightedDesign", function(object) {
   TRUE
 })
 
-# Internal function to use unitOfAssignmentIds to update the design with new variable
-# names
-.update_unitOfAssignmentIds <- function(design, data, unitOfAssignmentIds) {
-  if (!is.list(unitOfAssignmentIds) ||
-        is.null(names(unitOfAssignmentIds)) ||
-        any(names(unitOfAssignmentIds) == "")) {
-    stop("unitOfAssignmentIds must be named list")
-  }
-  if (any(duplicated(names(unitOfAssignmentIds))) || any(duplicated(unitOfAssignmentIds))) {
-    stop("unitOfAssignmentIds must be unique")
-  }
-
-  # Ensure all names and replacements are valid
-  missingnames <- !(names(unitOfAssignmentIds) %in% colnames(design@structure))
-  if (any(missingnames)) {
-    warning(paste("unitOfAssignmentIds labels not found in Design. unknown elements:",
-                  paste(names(unitOfAssignmentIds)[missingnames], collapse = ", ")))
-  }
-  missingdata <-  !(unitOfAssignmentIds %in% colnames(data))
-  if (any(missingdata)) {
-    warning(paste("unitOfAssignmentIds replacement values not found in data. unknown elements:",
-                  paste(unitOfAssignmentIds[missingnames], collapse = ", ")))
-  }
-
-  # if we have any names or replacements missing in the design or data,
-  # there's a warning, and then don't try to replace that element
-  unitOfAssignmentIds <- unitOfAssignmentIds[!missingnames & !missingdata]
-
-  newnames <- vapply(colnames(design@structure), function(x) {
-    pos <- names(unitOfAssignmentIds) == x
-    if (any(pos)) {
-      return(unitOfAssignmentIds[[which(pos)]])
-    }
-    return(x)
-  }, "character")
-
-  colnames(design@structure) <- newnames
-  return(design)
-}
-
 ##' @title Show a WeightedDesign
 ##' @param object WeightedDesignDesign object
 ##' @return an invisible copy of `object`
