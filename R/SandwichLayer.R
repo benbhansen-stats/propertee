4c149f318b781c44f3eff8afdb5b61705229b11f
jwasserman2 <joshua.wasserman777@gmail.com>
2022-05-05 11:18:45 -0400

    ADD presandwichlayer without 'by' functionality

--- R/SandwichLayer.R
+++ R/SandwichLayer.R
@@ -47,3 +47,57 @@ setValidity("SandwichLayer", function(object) {
   TRUE
 })
 
+PreSandwichLayer <- setClass("PreSandwichLayer",
+                             contains = "numeric",
+                             slots = c(fitted_covariance_model = "ANY",
+                                       prediction_gradient = "matrix"))
+
+setValidity("PreSandwichLayer", function(object) {
+  if (!("terms" %in% attr(object@fitted_covariance_model, "names"))) {
+    return("Fitted covariance model must have a 'terms' attribute")
+  }
+  
+  sandwich_env <- new.env()
+  assign("bread", sandwich::bread, envir=sandwich_env)
+  assign("estfun", sandwich::estfun, envir=sandwich_env)
+  if (is.null(utils::getS3method("bread",
+                                 class(object@fitted_covariance_model),
+                                 optional = T,
+                                 envir = sandwich_env)) |
+      is.null(utils::getS3method("estfun",
+                                 class(object@fitted_covariance_model),
+                                 optional = T,
+                                 envir = sandwich_env))) {
+    return("Functions for extracting vcov elements not applicable to fitted covariance model")
+  }
+  
+  if (!is.numeric(object@prediction_gradient)) {
+    return("Prediction gradient must be a numeric matrix")
+  }
+  if (dim(object@prediction_gradient)[1] != length(object)) {
+    return("Prediction gradient does not have the same number of rows as the offset")
+  }
+  if (dim(object@prediction_gradient)[2] != length(coef(object@fitted_covariance_model))) {
+    return(paste0("Prediction gradient does not have the same number of columns as ",
+                  "predictors in the covariance model"))
+  }
+  TRUE
+})
+
+setMethod("as.SandwichLayer", "PreSandwichLayer", function(object, design, by=NULL) {
+  missing_des_cols <- setdiff(colnames(design@structure),
+                              colnames(eval(object@fitted_covariance_model$call$data)))
+  for (col in missing_des_cols) {
+    object@fitted_covariance_model$call$data[col] <- NA_integer_
+  }
+  
+  keys <- model.matrix(object@fitted_covariance_model)
+  keys <- expand.model.frame(object@fitted_covariance_model,
+                             colnames(design@structure),
+                             na.expand = T)[colnames(design@structure)]
+  
+  return(SandwichLayer(object@.Data,
+                       fitted_covariance_model = object@fitted_covariance_model,
+                       prediction_gradient = object@prediction_gradient,
+                       keys = keys))
+})
