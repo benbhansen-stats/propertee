431fcc8ef1f2b0e1d5a87fcd12e2977e06f5f6b1
jwasserman2 <joshua.wasserman777@gmail.com>
2022-05-05 10:34:36 -0400

    TST tests pass with adding categorical cov

--- tests/testthat/test.SandwichLayer.R
+++ tests/testthat/test.SandwichLayer.R
@@ -1,16 +1,18 @@
 test_that("Sandwich Layer validity", {
   set.seed(20)
-  x <- rnorm(100)
-  y <- x + rnorm(100)
+  cont_x <- rnorm(100)
+  cat_x <- rbinom(100, 2, 0.2)
+  y <- cont_x - cat_x + rnorm(100)
   xstar <- data.frame("uoa1" = c(rep(1, 50), rep(2, 50)),
                       "uoa2" = rep(c(rep(1, 25), rep(2, 25)), 2),
                       "t" = c(rep(1, 25), rep(0, 50), rep(1, 25)),
-                      "x" = rnorm(100))
+                      "cont_x" = rnorm(100),
+                      "cat_x" = rbinom(100, 2, 0.2))
   
-  valid_cmod <- lm(y ~ x)
+  valid_cmod <- lm(y ~ cont_x + as.factor(cat_x))
   invalid_cmod <- list("a" = c(1, 2, 3))
   offset <- stats::predict(valid_cmod, xstar)
-  pred_gradient <- matrix(x, ncol = 1)
+  pred_gradient <- as.matrix(cbind(cont_x, cat_x))
   keys <- cbind(xstar[, c("uoa1", "uoa2", "t")], "offset" = offset)
   
   # covariance model incompatible with model.matrix
@@ -30,11 +32,11 @@ test_that("Sandwich Layer validity", {
                    keys = keys),
                "extracting vcov elements not applicable")
   
-  # prediction gradient is not a matrix
+  # prediction gradient is not a numeric matrix
   expect_error(new("SandwichLayer",
                    offset,
                    fitted_covariance_model = valid_cmod,
-                   prediction_gradient = matrix(as.character(x), ncol = 1),
+                   prediction_gradient = matrix(as.character(cbind(cont_x, cat_x)), ncol = 2),
                    keys = keys),
                "must be a numeric matrix")
   
@@ -42,7 +44,7 @@ test_that("Sandwich Layer validity", {
   expect_error(new("SandwichLayer",
                    offset,
                    fitted_covariance_model = valid_cmod,
-                   prediction_gradient = matrix(x[2:100], ncol = 1),
+                   prediction_gradient = as.matrix(cbind(cont_x, cat_x)[2:100,]),
                    keys = keys),
                "same number of rows")
   
