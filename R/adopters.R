44674154a68d01ee2797d48b1aa911530ba01c7f
Josh Errickson <josh@errickson.net>
2022-05-07 20:52:03 -0400

    Added some documentation

--- R/adopters.R
+++ R/adopters.R
@@ -1,6 +1,8 @@
-##' @title Adopters
-##' @param design Optional design
-##' @return Treatment
+##' @title Obtain Treatment from Design
+##' @param design Optional design. If \code{NULL}, the Design will be identified
+##'   from its inclusion in a weight (\code{ate()} or \code{ett()}) or from a
+##'   covariate adjustment model (\code{cov_adj()}).
+##' @return The treatment variable to be placed in the regression formula.
 ##' @export
 adopters <- function(design = NULL) {
   if (is.null(design)) {
@@ -20,6 +22,8 @@ adopters <- function(design = NULL) {
   treatment <- tryCatch(treatment_data[, var_names(design, "t")],
                         error = function(e) {
                           treatment_data[, paste0(var_names(design, "t"), ".y")]
+                          # if treatment variable already exists in data, there
+                          # will be a .x and .y version; e.g. z.x and z.y
                         })
 
   return(treatment)
