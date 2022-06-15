425b3d0728f0409f9b261915de17ce04385aa840
Josh Errickson <josh@errickson.net>
2022-06-15 14:33:25 -0400

    Documented more internal funtions

--- R/DesignAccessors.R
+++ R/DesignAccessors.R
@@ -65,8 +65,14 @@ setMethod("treatment<-", "Design", function(x, value) {
   x
 })
 
-# (Internal) Extracts treatment as binary vector if possible (if design has
-# dichotomy, or if treatment is 0/1), or else errors.
+##' If the \code{Design} has a dichotomy, or has a treatment variable consisting
+##' only of 0/1 or \code{NA}, then returns the binary treatment. Otherwise (it
+##' has a non-binary treatment and lacks a dichotomy) it errors.
+##' @title (Internal) Extracts treatment as binary vector if possible or else
+##'   errors.
+##' @param des A \code{Design}
+##' @return A vector of binary treatments
+##' @keywords internal
 .bin_txt <- function(des) {
   if (!is_dichotomized(des)) {
     tt <- treatment(des, binary = FALSE)[, 1]
@@ -77,6 +83,59 @@ setMethod("treatment<-", "Design", function(x, value) {
   }
   .binarize_treatment(treatment(des, binary = FALSE),
                       des@dichotomy)
+}
+
+##' @title (Internal) Uses a \code{@dichotomy} to create a binary version of the
+# treatment variable.
+##' @param trt A named \code{data.frame} containing a single column of the
+##'   treatment, such as that produed by `treatment(mydesign)`.
+##' @param dichot A dichotomization formula. See the details in the Details for
+##'   the help of \code{rct_design()}.
+##' @return A vector of binary treatments
+##' @keywords internal
+.binarize_treatment <- function(trt, dichot) {
+
+  if (!is(dichot, "formula")) {
+    stop("`dichotomy` must be formula")
+  }
+
+  if (!is.data.frame(trt)) {
+    stop(paste("`trt` is expected to be a named `data.frame`",
+               "(e.g. from `treatment(des)`)"))
+  }
+
+  lhs_dot <- rhs_dot <- FALSE
+  if (dichot[[3]] == ".") {
+    # control group is .
+    # control goes first since txt switches LHS and RHS
+    dichot[[3]] <- 1
+    rhs_dot <- TRUE
+  }
+  if (dichot[[2]] == ".") {
+    # treatment group is .
+    dichot[[2]] <- dichot[[3]]
+    dichot[[3]] <- 1
+    lhs_dot <- TRUE
+  }
+  if (lhs_dot & rhs_dot) {
+    stop("At least one side for dichotomy formula must not be `.`")
+  }
+
+  m <- model.frame(dichot, trt, na.action = na.pass)
+
+  if (lhs_dot) {
+    return(as.numeric(!m[,1]))
+  } else if (rhs_dot) {
+    return(as.numeric(m[,1]))
+  } else {
+    ditxt <- m[,2] + 2*m[,1] - 1
+    ditxt[ditxt == -1] <- NA
+    if (any(!is.na(ditxt) & ditxt > 1)) {
+      stop("treatment dichotomy overlaps")
+    }
+    return(ditxt)
+  }
+
 }
 
 ############### Units of Assignment
