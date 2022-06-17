b1f0679d9cb1d1023b58e42941078e02f1c7d8be
Josh Errickson <josh@errickson.net>
2022-06-17 12:05:25 -0400

    Add'l tests for #40

--- tests/testthat/test.lmitt.R
+++ tests/testthat/test.lmitt.R
@@ -46,3 +46,42 @@ test_that("covariate adjustment", {
   expect_true(is(da$model$"(offset)", "CovAdjPrediction"))
 
 })
+
+
+test_that("Design argument", {
+  data(simdata)
+  des <- obs_design(z ~ cluster(cid1, cid2), data = simdata)
+
+  mod1 <- lmitt(y ~ z, data = simdata, design = des, target = "ate")
+  mod2 <- lmitt(y ~ z, data = simdata, weights = ate(des))
+  mod3 <- lmitt(y ~ z, data = simdata, design = z ~ cluster(cid1, cid2),
+                target = "ate")
+  expect_true(mod3@Design@type == "Obs")
+  expect_identical(mod1@Design, mod2@Design)
+  expect_identical(mod1@Design, mod3@Design)
+
+  des2 <- rd_design(z ~ cluster(cid1, cid2) + forcing(force), data = simdata)
+
+  mod1 <- lmitt(y ~ z, data = simdata, design = des2, target = "ate")
+  mod2 <- lmitt(y ~ z, data = simdata, weights = ate(des2))
+  mod3 <- lmitt(y ~ z, data = simdata,
+                design = z ~ cluster(cid1, cid2) + forcing(force),
+                target = "ate")
+  expect_true(mod3@Design@type == "RD")
+  expect_identical(mod1@Design, mod2@Design)
+  expect_identical(mod1@Design, mod3@Design)
+
+  des3 <- obs_design(z ~ cluster(cid1, cid2) + block(bid), data = simdata)
+
+  mod1 <- lmitt(y ~ z, data = simdata, design = des3, target = "ate",
+                subset = simdata$dose < 300)
+  #mod2 <- lmitt(y ~ z, data = simdata, weights = ate(des3),
+  #              subset = simdata$dose < 300)
+  mod3 <- lmitt(y ~ z, data = simdata,
+                design = z ~ cluster(cid1, cid2) + block(bid),
+                target = "ate", subset = simdata$dose < 300)
+  expect_true(mod3@Design@type == "Obs")
+  #expect_identical(mod1@Design, mod2@Design)
+  expect_identical(mod1@Design, mod3@Design)
+
+})
