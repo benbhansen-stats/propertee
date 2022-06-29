4a29002d95e5ac1d7fc3d8ea1efde1feba188fd9
Josh Errickson <josh@errickson.net>
2022-06-29 13:58:54 -0400

    Rebuild ref site (updated some docs in service of the ref site as well)

--- R/SandwichLayerVariance.R
+++ R/SandwichLayerVariance.R
@@ -1,18 +1,20 @@
 #' @include Design.R SandwichLayer.R
 NULL
 
-#' @title (Internal) Get the B12 block of the sandwich variance estimator
-#' @details This block is the covariance matrix of the cluster-level estimating
-#' equations for the covariance and direct adjustment models. It has a row for
-#' each term in the covariance model and a column for each term in the direct
-#' adjustment model. For any row that does not appear in both the experimental
-#' design and the covariance model data, its contribution to this matrix will be
-#' 0. Thus, if there is no overlap between the two datasets, this will return a
-#' matrix of 0's.
+#' @title (Internal) Compute variance blocks
+#' @details The \bold{B12 block} is the covariance matrix of the cluster-level
+#'   estimating equations for the covariance and direct adjustment models. It
+#'   has a row for each term in the covariance model and a column for each term
+#'   in the direct adjustment model. For any row that does not appear in both
+#'   the experimental design and the covariance model data, its contribution to
+#'   this matrix will be 0. Thus, if there is no overlap between the two
+#'   datasets, this will return a matrix of 0's.
 #' @param x A \code{Lmitted} model
-#' @return A pxk matrix where p is the number of terms in the covariance model
-#' and k is the number of terms in the \code{Lmitted} model
+#' @return \code{.get_b12()}: A \eqn{p\times k} matrix where \eqn{p} is the
+#'   number of terms in the covariance model and \eqn{k} is the number of terms
+#'   in the \code{Lmitted} model
 #' @keywords internal
+#' @rdname sandwich_elements_calc
 .get_b12 <- function(x) {
   if (!inherits(x, "Lmitted")) {
     stop("x must be a Lmitted model")
@@ -61,21 +63,21 @@ NULL
   return(crossprod(cmod_eqns, dmod_eqns))
 }
 
-#' @title (Internal) Get the inverse of the A22 block of the sandwich variance estimator
-#' @param x A \code{Lmitted} model
-#' @details This block is the diagonal element of the inverse expected Fisher
-#' Information matrix corresponding to the treatment estimate. As shown in the
-#' Details of \code{\link{.get_b22}}, the estimating equations for a generalized
-#' linear model with a canonical link function can be written as
-#' \deqn{\psii = (ri / Var(yi)) * (d\mui/d\etai) * xi}
-#' The expected information matrix A22 is then the negative Jacobian of \eqn{\psii},
-#' which by likelihood theory is the variance-covariance matrix of \eqn{\psii}:
-#' \deqn{\psi_{i}\psi_{i}^{T} = E[(ri / Var(yi) * (d\mui/d\etai))^{2}] * xixi' =
-#' (d\mui/d\etai)^{2} / Var(yi) * xixi'} Considering the whole sample, this can be
-#' expressed in matrix form as \eqn{X'WX}. The output of this function is the
-#' inverse of the diagonal element corresponding to the treatment estimate.
-#' @return A 1x1 matrix
+#' @details The \bold{A22 block} is the diagonal element of the inverse expected
+#'   Fisher Information matrix corresponding to the treatment estimate. As shown
+#'   in the Details of \code{.get_b22()}, the estimating equations for a
+#'   generalized linear model with a canonical link function can be written as
+#'   \deqn{\psi_i = (r_i / Var(y_i)) * (d\mu_i/d\eta_i) * x_i} The expected
+#'   information matrix A22 is then the negative Jacobian of \eqn{\psi_i}, which
+#'   by likelihood theory is the variance-covariance matrix of \eqn{\psi_i}:
+#'   \deqn{\psi_{i}\psi_{i}^{T} = E[(r_i / Var(y_i) * (d\mu_i/d\eta_i))^{2}] *
+#'   x_ix_i' = (d\mu_i/d\eta_i)^{2} / Var(y_i) * x_ix_i'} Considering the whole
+#'   sample, this can be expressed in matrix form as \eqn{X'WX}. The output of
+#'   this function is the inverse of the diagonal element corresponding to the
+#'   treatment estimate.
+#' @return \code{.get_a22_inverse()}: A \eqn{1\times1} matrix
 #' @keywords internal
+#' @rdname sandwich_elements_calc
 .get_a22_inverse <- function(x) {
   if (!inherits(x, "Lmitted")) {
     stop("x must be a Lmitted model")
@@ -100,34 +102,35 @@ NULL
   return(out)
 }
 
-#' @title (Internal) Get the B22 block of the sandwich variance estimator
-#' @param x A \code{Lmitted} model
 #' @param ... Arguments to be passed to sandwich::meatCL
-#' @details This block refers to a clustered variance estimate of the treatment
-#' effect estimate. The \code{stats} package offers family objects with
-#' canonical link functions, so the log-likelihood for a generalized linear model
-#' can be written in terms of the linear predictor as \deqn{L(yi, \beta, \phi, wi)
-#' = wi * (yi * \beta'xi - b(\beta'xi)) / \phi + h(yi; \phi)} The estimating equations
-#' \eqn{\psii} given by the score function can then be expressed as
-#' \deqn{\psii = E[wi * (yi - \mu(\beta'xi)) * xi / \phi]}
-#' In section 4.4 of the second edition of Categorical Data Analysis, Agresti
-#' shows the derivative of the mean function with respect to the linear predictor
-#' is equivalent to the weighted variance for an observation divided by the estimate
-#' of the dispersion parameter. Thus, the above estimating equations can also be
-#' written as \deqn{\psii = (ri / Var(yi)) * (d\mui/d\etai) * xi}
+#' @details The \bold{B22 block} refers to a clustered variance estimate of the
+#'   treatment effect estimate. The \code{stats} package offers family objects
+#'   with canonical link functions, so the log-likelihood for a generalized
+#'   linear model can be written in terms of the linear predictor as
+#'   \deqn{L(y_i, \beta, \phi, w_i) = w_i * (y_i * \beta'x_i - b(\beta'x_i)) /
+#'   \phi + h(y_i; \phi)} The estimating equations \eqn{\psi_i} given by the
+#'   score function can then be expressed as \deqn{\psi_i = E[w_i * (y_i -
+#'   \mu(\beta'x_i)) * x_i / \phi]} In section 4.4 of the second edition of
+#'   Categorical Data Analysis, Agresti shows the derivative of the mean
+#'   function with respect to the linear predictor is equivalent to the weighted
+#'   variance for an observation divided by the estimate of the dispersion
+#'   parameter. Thus, the above estimating equations can also be written as
+#'   \deqn{\psi_i = (r_i / Var(y_i)) * (d\mu_i/d\eta_i) * x_i}
 #'
-#' A matrix C of dimension nxJ is formed to indicate which unit each subject in
-#' the design belongs to, where J is the number of units at the level of treatment
-#' assignment. The treatment assignment-level estimating equations are then obtained
-#' via \eqn{C'\psi}, where \eqn{\psi} is the matrix of estimating equations at the
-#' subject level.
+#'   A matrix \eqn{C} of dimension \eqn{n\times J} is formed to indicate which
+#'   unit each subject in the design belongs to, where \eqn{J} is the number of
+#'   units at the level of treatment assignment. The treatment assignment-level
+#'   estimating equations are then obtained via \eqn{C'\psi}, where \eqn{\psi}
+#'   is the matrix of estimating equations at the subject level.
 #' @references Agresti, Alan. Categorical Data Analysis. 2003. Open WorldCat,
-#' https://nbn-resolving.org/urn:nbn:de:101:1-201502241089.
-#' @return A (p+1)x(p+1) matrix where the dimensions are given by the number of
-#' terms in the \code{Lmitted} model (p) and an Intercept term. Typically, this should
-#' be a 2x2 matrix given the dichotomous handling of treatment variables in this
-#' package and the use of the covariance model to offer the covariance adjustment.
+#'   https://nbn-resolving.org/urn:nbn:de:101:1-201502241089.
+#' @return \code{.get_b22()}: A \eqn{(p+1)\times(p+1)} matrix where the
+#'   dimensions are given by the number of terms in the \code{Lmitted} model
+#'   (\eqn{p}) and an Intercept term. This should be a \eqn{2\times2} matrix
+#'   given the dichotomous handling of treatment variables in this package and
+#'   the use of the covariance model to offer the covariance adjustment.
 #' @keywords internal
+#' @rdname sandwich_elements_calc
 .get_b22 <- function(x, ...) {
   if (!inherits(x, "Lmitted")) {
     stop("x must be a Lmitted model")
@@ -151,16 +154,17 @@ NULL
   return(out)
 }
 
-#' @title (Internal) Get the inverse of the A11 block of the sandwich variance estimator
-#' @param x A \code{Lmitted} model
-#' @details This block is the pxp matrix corresponding to the unscaled
-#' inverse of the observed Fisher information of the covariance model. The
-#' observed information is given by the estimate of the negative Jacobian of the
-#' model's estimating equations. The unscaled version provided here divides by
-#' the number of observations used to fit the covariance model.
-#' @return a (p+1)x(p+1) matrix where the dimensions are given by the number of
-#' terms in the covariance model (p) and an Intercept term.
+#' @details The \bold{A11 block} is the \eqn{p\times p} matrix corresponding to
+#'   the unscaled inverse of the observed Fisher information of the covariance
+#'   model. The observed information is given by the estimate of the negative
+#'   Jacobian of the model's estimating equations. The unscaled version provided
+#'   here divides by the number of observations used to fit the covariance
+#'   model.
+#' @return \code{.get_a11_inverse()}: A \eqn{(p+1)\times(p+1)} matrix where the
+#'   dimensions are given by the number of terms in the covariance model
+#'   (\eqn{p}) and an Intercept term.
 #' @keywords internal
+#' @rdname sandwich_elements_calc
 .get_a11_inverse <- function(x) {
   if (!inherits(x, "Lmitted")) {
     stop("x must be a Lmitted model")
@@ -179,18 +183,18 @@ NULL
   return(out)
 }
 
-#' (Internal) Get the B11 block of the sandwich variance estimator
-#' @param x A \code{Lmitted} model
-#' @param ... Arguments to be passed to sandwich::meatCL
-#' @details This is the block of the sandwich variance estimator corresponding to
-#' the variance-covariance matrix of the covariance model coefficient estimates.
-#' The estimates returned here are potentially clustered (by the clustering in the
-#' experimental design) if rows in the covariance model data also exist in the
-#' design. If there is no overlap between the two datasets, the variance-covariance
-#' matrix is estimated assuming the observations are independent.
-#' @return A (p+1)x(p+1) matrix the dimensions are given by the number of
-#' terms in the covariance model (p) and an Intercept term
+#' @details The \bold{B11 block} is the block of the sandwich variance estimator
+#'   corresponding to the variance-covariance matrix of the covariance model
+#'   coefficient estimates. The estimates returned here are potentially
+#'   clustered (by the clustering in the experimental design) if rows in the
+#'   covariance model data also exist in the design. If there is no overlap
+#'   between the two datasets, the variance-covariance matrix is estimated
+#'   assuming the observations are independent.
+#' @return \code{.get_b11()}: A \eqn{(p+1)\times(p+1)} matrix the dimensions are
+#'   given by the number of terms in the covariance model (\eqn{p}) and an
+#'   Intercept term
 #' @keywords internal
+#' @rdname sandwich_elements_calc
 .get_b11 <- function(x, ...) {
   if (!inherits(x, "Lmitted")) {
     stop("x must be a Lmitted model")
