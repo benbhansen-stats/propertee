6f985b122d2a5337e73db4ae95eb0c2034805ccf
Josh Errickson <josh@errickson.net>
2022-07-21 20:15:40 -0400

    Cleaned up PR
    Added tests, updated documentation, cleaned up code

--- R/cov_adj.R
+++ R/cov_adj.R
@@ -1,9 +1,17 @@
 #' @include SandwichLayer.R
 NULL
 
-##' @title Covariance Adjustment for Treatment Estimation
-##' @param model Any model that inherits from a \code{glm}, \code{lm}, or \code{
-##' robustbase::lmrob} object
+##' Covariance Adjustment for Treatment Estimation
+##'
+##' Prior to obtaining predicted values, \code{cov_adj()} tries to identify the
+##' treatment variable (as specified in the \code{design}) and replace it with
+##' the reference level. If the treatment is binary, this is \code{FALSE}. If
+##' treatment is numeric, it is the smallest non-negative value (note that this
+##' means for 0/1 binary, it uses a 0). Factor treatments are not currently
+##' supported, but if we add them, it will use the first \code{level()} of the
+##' factor, you may change this by using \code{relevel()} to adjust.
+##' @param model Any model that inherits from a \code{glm}, \code{lm}, or
+##'   \code{robustbase::lmrob} object
 ##' @param newdata Optional; a data.frame of new data
 ##' @param design Optional \code{Design}.
 ##' @return Covariate adjusted outcomes
@@ -15,29 +23,32 @@ cov_adj <- function(model, newdata = NULL, design =  NULL) {
     newdata <- tryCatch(
       .get_data_from_model("weights", form),
        error = function(e) {
-         warning(paste("Could not find quasiexperimental data in the call stack,",
-                       "using the covariance model data to generate the covariance",
-                       "adjustments"))
+         warning(paste("Could not find quasiexperimental data in the call",
+                       "stack, using the covariance model data to generate",
+                       "the covariance adjustments"))
          stats::model.frame(model)
        })
   }
 
-  if (is.null(design)) { ### I moved this up here
+  if (is.null(design)) {
     design <- .get_design(NULL_on_error = TRUE)
   }
 
-  if(!is.null(design)) ### added this
-    ##if(design@type=='RD') ## I think we want to set Z=0 for all design types, right?
-  {
+  if (!is.null(design)) {
     trt_name <- var_names(design,'t')
-    if(trt_name %in% names(newdata))
-      if(is.numeric(newdata[[trt_name]])){
-        newdata[[trt_name]] <- 0 ## are the scenarios where 0 is not the control value?
-      } else if(is.logical(newdata[[trt_name]])){
+    if (trt_name %in% names(newdata))
+      if (is.numeric(treatment(design)[, 1])) {
+        newdata[[trt_name]] <- min(abs(treatment(design)[, 1]))
+      } else if (is.logical(treatment(design)[, 1])) {
         newdata[[trt_name]] <- FALSE
-      } else warning(paste("The treatment variable is in the covariance adjustment model,",
-                           "and is neither logical or numeric; for now, partial residuals",
-                           "only implemented for logical or numeric treatments"))
+      } else if (is.factor(treatment(design)[, 1])) {
+        newdata[[trt_name]] <- levels(treatment(design)[, 1])[1]
+      } else {
+        warning(paste("The treatment variable is in the covariance adjustment",
+                      "model, and is neither logical or numeric; for now,",
+                      "partial residuals only implemented for logical or",
+                      "numeric treatments"))
+      }
   }
 
   ca_and_grad <- .get_ca_and_prediction_gradient(model, newdata)
