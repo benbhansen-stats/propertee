35fe24d4ba48d6a21bbbb1e73bdf20509f7416ba
Josh Errickson <josh@errickson.net>
2023-03-29 06:12:06 -0400

    Adjusted expand.model.frame difference test

--- tests/testthat/test.notforCRAN.R
+++ tests/testthat/test.notforCRAN.R
@@ -2,11 +2,12 @@ test_that("expand.model.frame changes", {
   # Since the .expand.model.frame.DA is a clone of stats::expand.model.frame,
   # this "test" captures if stats::expand.model.frame changes. See issue #102.
 
-  fcode <- capture.output(.expand.model.frame.DA)
-  scode <- capture.output(expand.model.frame)
+  fcode <- capture.output(flexida:::.expand.model.frame.DA)
+  scode <- capture.output(stats::expand.model.frame)
 
   # Remove environment flags
   scode <- scode[!grepl("bytecode:", scode)]
+  fcode <- fcode[!grepl("bytecode:", fcode)]
   scode <- scode[-length(scode)]
   fcode <- fcode[-length(fcode)]
 
@@ -22,6 +23,9 @@ test_that("expand.model.frame changes", {
   # Strip preceeding or trailing spaces
   fcode <- gsub("[[:space:]]+", "", fcode)
   scode <- gsub("[[:space:]]+", "", scode)
+
+  # For debugging purposes, this will do a character-wise comparison:
+  ## print(Reduce(`==`, strsplit(c(fcode, scode), "")))
 
   if (fcode != scode) {
     stop("stats::expand.model.frame may have been changed! See Issue #102")
