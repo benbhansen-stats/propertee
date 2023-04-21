c6017e636ea9b7d5180343f7c4b4909ba6147c28
jwasserman2 <joshua.wasserman777@gmail.com>
2023-04-20 23:56:14 -0400

    MAINT remove code for handling rank deficiency in a22-inverse calculation

--- R/SandwichLayerVariance.R
+++ R/SandwichLayerVariance.R
@@ -194,11 +194,7 @@ vcovDA <- function(x, type = c("CR0", "MB0", "HC0"), cluster = NULL, ...) {
 
   # Get expected information per sandwich_infrastructure vignette
   w <- if (is.null(x$weights)) 1 else x$weights
-  # NOTE: summary.lm handles less than full rank design matrices by taking the first
-  # columns that meet column rank. We will want to be more specific given the
-  # effects we want to report
-  model.rank <- x$rank
-  out <- solve(crossprod(stats::model.matrix(x) * sqrt(w))[1L:model.rank, 1L:model.rank])
+  out <- solve(crossprod(stats::model.matrix(x) * sqrt(w)))
 
   return(out)
 }
