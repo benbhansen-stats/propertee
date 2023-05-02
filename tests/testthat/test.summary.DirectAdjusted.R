d87656da3b1860dd5d4b205b16638a57f5b91ce8
Josh Errickson <josh@errickson.net>
2023-05-02 11:00:39 -0400

    FIxed failing tests

--- tests/testthat/test.summary.DirectAdjusted.R
+++ tests/testthat/test.summary.DirectAdjusted.R
@@ -32,13 +32,16 @@ test_that("DirectAdjusted with SandwichLayer offset summary uses vcovDA SE's", {
                       lower.tail = FALSE))
 
   cmod <- lm(y ~ x, simdata, subset = z == 0)
-  damod <- lmitt(lm(y ~ assigned(), data = simdata, weights = ate(des),
+  # Removing intercept which is otherwise estimated at 0, causing warnings in
+  # sqrt(diag(vcov)) calculations below
+  damod <- lmitt(lm(y ~ assigned() + 0, data = simdata, weights = ate(des),
                     offset = cov_adj(cmod)))
+
   s <- summary(damod)
-  expect_equal(s$coefficients[, 2L], sqrt(diag(vcovDA(damod))))
-  expect_equal(s$coefficients[, 3L],
+  expect_true(s$coefficients[, 2L] ==  sqrt(diag(vcovDA(damod))))
+  expect_true(s$coefficients[, 3L] ==
                damod$coefficients / sqrt(diag(vcovDA(damod))))
-  expect_equal(s$coefficients[, 4L],
+  expect_true(s$coefficients[, 4L] ==
                2 * pt(abs(damod$coefficients / sqrt(diag(vcovDA(damod)))),
                       damod$df.residual,
                       lower.tail = FALSE))
