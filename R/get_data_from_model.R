25229dc0a479fbe38e6f32b78c286558627d9275
jwasserman2 <joshua.wasserman777@gmail.com>
2023-05-02 13:45:48 -0400

    ENH don't change .fallback_data_search()

--- R/get_data_from_model.R
+++ R/get_data_from_model.R
@@ -180,11 +180,10 @@
 ##'   call stack.
 ##' @return If found, the data.
 ##' @keywords internal
-.fallback_data_search <- function(data_name = NULL) {
-  if (is.null(data_name)) data_name <- "data"
+.fallback_data_search <- function() {
   for (i in seq_len(sys.nframe())) {
     try({
-      data <- get(data_name, envir = sys.frame(i))
+      data <- get("data", envir = sys.frame(i))
     }, silent = TRUE)
     if (!is.null(data) && is.data.frame(data)) {
       break()
