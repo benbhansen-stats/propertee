ab7251adee083c6ed4f1858cbc467a5fa9fc9bf4
Josh Errickson <josh@errickson.net>
2024-05-08 09:58:54 -0400

    Move test out of temp file #125

---
+++ tests/testthat/test.estfun.glmrob.R
@@ -0,0 +1,25 @@
+if (requireNamespace("robustbase", quietly = TRUE)) {
+  test_that("the columns of estfun.glmrob have approximately 0 mean",{
+    data(lsoSynth)
+
+    mod <- robustbase::glmrob(probation_year1~age_at_entry+lhsgrade_pct+male,
+                  family=binomial,data=lsoSynth)
+
+
+    ee <- estfun(mod)
+
+    expect_true(all(abs(colMeans(ee))<0.001))
+  })
+
+  test_that("the sandwich vcov matrix for a glmrob object is approximately the same as the cov returned with the object",{
+    data(lsoSynth)
+
+    mod <- robustbase::glmrob(probation_year1~age_at_entry+lhsgrade_pct+male,
+                  family=binomial,data=lsoSynth)
+
+    expect_true(max(abs(sandwich(mod)-mod$cov))<0.01)
+  })
+} else {
+  expect_true(TRUE)
+  # Dummy test to avoid empty test warnings
+}
