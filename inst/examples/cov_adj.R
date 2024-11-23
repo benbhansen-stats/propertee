0d50d27ae60a77fc622c0a469f510f446b6552d1
Ben B. Hansen <bbh@umich.edu>
2024-11-23 05:19:13 -0500

    Update cov_adj examples using STAR data
    Simplify presentation with use of `assigned()`,
    `dichotomy=`, `as.lmitt()` and `lmitt.formula()`,
    which seem to have been unavailable when the example
    was written. As of this commit there is a problem in
    some of the demonstrated calculations; see #194.

--- inst/examples/cov_adj.R
+++ inst/examples/cov_adj.R
@@ -1,30 +1,36 @@
 data(STARdata)
-STARdata$treatment <- STARdata$stark == "small"
-STARdata$treatment[is.na(STARdata$treatment)] <- FALSE
-STARdata$studentid <- as.character(seq_len(nrow(STARdata)))
+STARdata$studentID <- rownames(STARdata)
 
-covariance_y0_read <- lm(readk ~ gender + ethnicity + lunchk +
-                                 ladderk + experiencek + tethnicityk,
-                         data = STARdata, subset = !treatment)
+y0hat_read <- lm(readk ~ gender + ethnicity + lunchk +
+                     ladderk + experiencek + tethnicityk,
+                 data = STARdata, subset = stark !="small")
 
-STARdata_spec <- rct_spec(treatment ~ cluster(studentid), data = STARdata)
-STARdata_ate    <- ate(STARdata_spec, data = STARdata)
-STARdata_ett    <- ett(STARdata_spec, data = STARdata)
+STARspec <- rct_spec(stark ~ unit_of_assignment(studentID),
+                     data = STARdata)
+ett_wts    <- ett(STARspec, data = STARdata,
+                  dichotomy= stark =="small" ~.)
 
-ett_read <- lm(readk ~ treatment,
-               offset = cov_adj(covariance_y0_read, newdata = STARdata),
+ett_read <- lm(readk ~ assigned(dichotomy= stark =="small" ~.),
+               ### expect warning about NAs generated here:
+               offset = cov_adj(y0hat_read, newdata = STARdata),
                data = STARdata,
-               weights = STARdata_ett)
+               weights = ett_wts)
 coef(ett_read)
+ett_read |> as.lmitt() |> vcov()
 
-ate_read <- lm(readk ~ treatment,
-               offset = cov_adj(covariance_y0_read, newdata = STARdata),
-               data = STARdata,
-               weights = STARdata_ate)
+ate_read <- lmitt(readk ~ 1, STARspec, STARdata,
+                  dichotomy= stark =="small" ~., 
+                  offset = cov_adj(y0hat_read, newdata = STARdata),
+                  weights = "ate")
+show(ate_read)
 coef(ate_read)
+vcov(ate_read)
 
-ate_read_eth <- lm(readk ~ treatment * ethnicity,
-                   offset = cov_adj(covariance_y0_read, newdata = STARdata),
-                   data = STARdata,
-                   weights = STARdata_ate)
-coef(ate_read_eth)
+ate_read_loc <-
+    lmitt(readk ~ schoolk, STARspec, STARdata,
+          dichotomy= stark =="small" ~., 
+          offset = cov_adj(y0hat_read, newdata = STARdata),
+          weights = "ate")
+show(ate_read_loc)
+
+
