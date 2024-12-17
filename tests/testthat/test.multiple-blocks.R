c4de25ff8624cf6df8ab6c3059a839931ec36d9d
Josh Errickson <josh@errickson.net>
2024-12-17 06:34:23 -0500

    #208 test for 3 variables; checking higher dimensional tables

--- tests/testthat/test.multiple-blocks.R
+++ tests/testthat/test.multiple-blocks.R
@@ -1,4 +1,4 @@
-test_that("Same result with one or two variable for blocks", {
+test_that("Same result with multiple variable for blocks", {
   data(simdata)
   spec1 <- rct_spec(z ~ uoa(uoa1, uoa2) + block(bid), data = simdata)
 
@@ -27,5 +27,28 @@ test_that("Same result with one or two variable for blocks", {
   coeff2 <- summary(mod2)$coeff
 
   all(coeff1 == coeff2)
+
+  simdata3 <- simdata
+  simdata3$bid1 <- simdata$bid == 1
+  simdata3$bid2 <- simdata$bid == 2
+  simdata3$bid3 <- simdata$bid == 3
+
+  # Make sure block info is same, only diagonal entries of cross table between
+  # original blocks and new blocks should have values.
+  expect_true(sum(diag(with(simdata3, table(bid, paste(bid3, bid2, bid1))))) == nrow(simdata3))
+  # Changed order of bid3-bid1 because of sorting
+
+  spec3 <- rct_spec(z ~ uoa(uoa1, uoa2) + block(bid1, bid2, bid3), data = simdata3)
+  expect_true(ncol(blocks(spec3)) == 3)
+
+  wts3 <- ate(spec3, data = simdata3)
+
+  expect_true(all(wts1 == wts3))
+
+  mod3 <- lmitt(y ~ x, data = simdata3, spec = spec3, weights = wts3)
+  coeff3 <- summary(mod3)$coeff
+
+  all(coeff1 == coeff3)
+
 
 })
