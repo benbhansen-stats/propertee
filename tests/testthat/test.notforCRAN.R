f97186c1893f96bd8f7a2a15a1f996bb75de128c
Josh Errickson <jerrick@umich.edu>
2025-01-30 09:17:43 -0500

    Running tidyverse tests only if tidyverse installed

--- tests/testthat/test.notforCRAN.R
+++ tests/testthat/test.notforCRAN.R
@@ -1,16 +1,21 @@
 test_that("#137 issues with grouped_df", {
-  suppressMessages(library(tidyverse))
-  data(simdata)
+  if (require(tidyverse, quietly = TRUE)) {
+    data(simdata)
 
-  # The failure we were seeing was due to the continuous moderator being renamed
-  # through tidyverse. This should no longer error.
-  mod <- lmitt(y ~ o, data = group_by(simdata, z), specification = z ~ cluster(uoa1, uoa2))
-  expect_s4_class(mod, "teeMod")
+    # The failure we were seeing was due to the continuous moderator being renamed
+    # through tidyverse. This should no longer error.
+    mod <- lmitt(y ~ o, data = group_by(simdata, z), specification = z ~ cluster(uoa1, uoa2))
+    expect_s4_class(mod, "teeMod")
+  } else {
+    # Avoid warnings about empty tests
+    expect_true(TRUE)
+  }
+
 })
 
 
 test_that("#174 non-data-frame input handled", {
-  suppressMessages(library(tidyverse))
+  if (require(tidyverse, quietly = TRUE)) {
   data(simdata)
 
   expect_error(rct_spec(z ~ uoa(uoa1, uoa2), data = y ~ x),
@@ -22,4 +27,8 @@ test_that("#174 non-data-frame input handled", {
   spec2 <- rct_spec(z ~ uoa(uoa1, uoa2), data = sd_tibble)
   spec1@call <- spec2@call
   expect_identical(spec1, spec2)
+  } else {
+    # Avoid warnings about empty tests
+    expect_true(TRUE)
+  }
 })
