249816d5639fb441d12e408a657e46d82531131c
Ben Hansen <bbh@umich.edu>
2025-06-26 17:22:45 -0400

    Bundle demographics w/ mi_sch_pairs data; update docs
    Adds the CCD demographic variables to the dataset and the CCD
    to the references list.   Also adds the external data URLs to the
    generating scripts, fills out documentation and minorly edits
    associated scripts.

--- data-raw/michigan_school_pairs.R
+++ data-raw/michigan_school_pairs.R
@@ -8,10 +8,12 @@ data_temp <- data.frame(
   z = c(0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0)
 )
 
-source("../propertee/vignettes/mi_vignette/get_and_clean_external_data.R")
+source("../vignettes/mi_vignette/get_and_clean_external_data.R")
 
 merged_data <- merge(analysis1data, data_temp, by = "schoolid")
 michigan_school_pairs <- merged_data[, c("schoolid", "blk", "z", "MALE_G11_PERC", "FEMALE_G11_PERC", "AM_G11_PERC", "ASIAN_G11_PERC", "HISP_G11_PERC", 
                             "BLACK_G11_PERC", "WHITE_G11_PERC", "PACIFIC_G11_PERC", "TR_G11_PERC","G11")]
+colnames(michigan_school_pairs)  <-
+    tolower(colnames(michigan_school_pairs))
 
 usethis::use_data(michigan_school_pairs, overwrite = TRUE)
